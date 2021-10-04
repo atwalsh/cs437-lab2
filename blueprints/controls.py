c129059ea7ec92032706ad42ea37266e2cc318d2
Adam Walsh <adamtwalsh@gmail.com>
2021-10-04 19:24:42 -0400

    Update control routes

--- blueprints/controls.py
+++ blueprints/controls.py
@@ -1,23 +1,31 @@
 from flask import Blueprint
 
+import picar_4wd as fc
+
 controls_bp = Blueprint('controls_bp', __name__, url_prefix='/controls')
+
+POWER_VAL = 25
 
 
 @controls_bp.route('/forward')
 def forward():
-    return 'forward'
+    fc.forward(POWER_VAL)
+    return 200
 
 
-@controls_bp.route('/back')
-def back():
-    return 'back'
+@controls_bp.route('/backward')
+def backward():
+    fc.backward(POWER_VAL)
+    return 200
 
 
 @controls_bp.route('/left')
 def left():
-    return 'left'
+    fc.turn_left(POWER_VAL)
+    return 200
 
 
 @controls_bp.route('/right')
 def right():
-    return 'right'
+    fc.turn_right(POWER_VAL)
+    return 200
