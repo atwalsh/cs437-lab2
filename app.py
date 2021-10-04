b8df1d9730afb8af1003c0b39b2b89597e027632
Adam Walsh <adamtwalsh@gmail.com>
2021-10-04 19:19:31 -0400

    Register `data_bp`

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from blueprints.controls import controls_bp
+from blueprints.data import data_bp
 
 if __name__ == '__main__':
     app = Flask(__name__)
 
     # Register blueprints
     app.register_blueprint(controls_bp)
+    app.register_blueprint(data_bp)
 
     # Run server
     app.run(debug=True, host='0.0.0.0')
