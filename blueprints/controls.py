2f6c8f15338795e714653f9fe68dd1b7ad0b9cc6
Adam Walsh <adamtwalsh@gmail.com>
2021-10-04 19:26:43 -0400

    Add `stop` control

--- blueprints/controls.py
+++ blueprints/controls.py
@@ -7,25 +7,31 @@ controls_bp = Blueprint('controls_bp', __name__, url_prefix='/controls')
 POWER_VAL = 25
 
 
+@controls_bp.route('/stop')
+def stop():
+    fc.stop()
+    return '', 200
+
+
 @controls_bp.route('/forward')
 def forward():
     fc.forward(POWER_VAL)
-    return 200
+    return '', 200
 
 
 @controls_bp.route('/backward')
 def backward():
     fc.backward(POWER_VAL)
-    return 200
+    return '', 200
 
 
 @controls_bp.route('/left')
 def left():
     fc.turn_left(POWER_VAL)
-    return 200
+    return '', 200
 
 
 @controls_bp.route('/right')
 def right():
     fc.turn_right(POWER_VAL)
-    return 200
+    return '', 200
