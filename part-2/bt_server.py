01c2e65413f296d07dec0fc6bff8bc87c36e0a89
Nick Brown <nwbrown@gmail.com>
2021-10-10 20:12:05 -0400

    Add config and fix a few things

--- part-2/bt_server.py
+++ part-2/bt_server.py
@@ -2,20 +2,16 @@
 Bluetooth server script to run on the Pi.
 """
 import bluetooth
-from picar_4wd import Ultrasonic, Pin
+from picar_4wd import Ultrasonic, Pin, us
 from picar_4wd.utils import getIP, cpu_temperature, cpu_usage
 import picar_4wd as fc
+import yaml
+import argparse
 
 hostMACAddress = "E4:5F:01:2D:6E:FB"
 port = 0
 backlog = 1
 size = 1024
-
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMACAddress, port))
-s.listen(backlog)
-us = Ultrasonic(Pin('D8'), Pin('D9'))
-print("Listening on port ", port)
 
 
 def get_stats():
@@ -46,6 +42,8 @@ def stop():
     fc.stop()
     return 'Stopping the car'
 
+def help():
+    return "\n".join(commands)
 
 commands = {
     'stats': get_stats,
@@ -53,21 +51,40 @@ commands = {
     'backward': backward,
     'left': left,
     'right': right,
-    'stop': stop
+    'stop': stop,
+    'help': help
 }
 
-try:
-    client, clientInfo = s.accept()
-    print(f'Connected to {clientInfo[0]}')
-    while 1:
-        data = client.recv(size)
-        if data:
-            data_str = data.decode()
-            if data_str in commands:
-                client.send(commands[data_str]().encode())
-            else:
-                client.send('Unknown command'.encode())
-except:
-    print("Closing socket")
-    client.close()
-    s.close()
+def run(host):
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.bind((host, port))
+    s.listen(backlog)
+    us = Ultrasonic(Pin('D8'), Pin('D9'))
+    print("Listening on port ", port)
+
+
+    try:
+        client, clientInfo = s.accept()
+        print(f'Connected to {clientInfo[0]}')
+        while 1:
+            data = client.recv(size)
+            if data:
+                data_str = data.decode()
+                if data_str in commands:
+                    client.send(commands[data_str]().encode())
+                else:
+                    client.send('Unknown command'.encode())
+    except Exception as e:
+        print(f"Error {e}")
+        print("Closing socket")
+        client.close()
+        s.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Client app for car control")
+    parser.add_argument("--config", help="config file", default="config.yaml")
+    args = parser.parse_args()
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+    host = config['server']['address']
+    run(host)
