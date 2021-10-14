138c2a9441b3bb95cb443edb31d449d8223798a0
Nick Brown <nwbrown@gmail.com>
2021-10-14 01:04:29 -0400

    Serve index

--- part-3/app.py
+++ part-3/app.py
@@ -1,16 +1,22 @@
 from flask import Flask
+from flask.helpers import send_file
+from flask.templating import render_template
 
 from blueprints.controls import controls_bp
 from blueprints.data import data_bp
 from blueprints.camera import camera_bp
 
+app = Flask(__name__)
+# Register blueprints
+app.register_blueprint(controls_bp)
+app.register_blueprint(data_bp)
+app.register_blueprint(camera_bp)
+
+@app.route("/")
+def index():
+    print("Rendering index")
+    return send_file("static/index.html")
+
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    # Register blueprints
-    app.register_blueprint(controls_bp)
-    app.register_blueprint(data_bp)
-    app.register_blueprint(camera_bp)
-
     # Run server
     app.run(debug=True, host='0.0.0.0')
